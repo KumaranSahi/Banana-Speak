2540658ccfa212ed7d3d7161b0001ed243da194e
Muthu Kumaran <61352185+KumaranSahi@users.noreply.github.com>
2020-11-30 14:26:46 +0530

    fix-Cors error fix

--- scripts.js
+++ scripts.js
@@ -3,7 +3,7 @@ let translateButton=document.getElementById('translate-button');
 let outputElement=document.getElementById('output');
 
 translateButton.onclick=()=>{
-    fetch(`https://api.funtranslations.com/translate/minion.json?text=${inputTextElement.value}`)
+    fetch(`https://cors-anywhere.herokuapp.com/https://api.funtranslations.com/translate/minion.json?text=${inputTextElement.value}`)
     .then(response=>response.json())
     .then(response=>{
         outputElement.innerHTML=response.contents.translated
